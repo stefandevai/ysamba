1d29b9d477d531060e30ca7ebe3b0c322d6f8d6b
Stefan Devai <stefandevai@posteo.me>
2023-11-28 03:48:31 +0100

    Remove switch case from fragment shader

--- data/shaders/world.fs
+++ data/shaders/world.fs
@@ -19,43 +19,7 @@ void main()
   if (f_in.tid > -0.5)
   {
     int tid = int(f_in.tid + 0.5);
-
-    switch (tid)
-    {
-      case 0:
-        final_color = texture(textures[0], f_in.uv);
-        break;
-      case 1:
-        final_color = texture(textures[1], f_in.uv);
-        break;
-      case 2:
-        final_color = texture(textures[2], f_in.uv);
-        break;
-      case 3:
-        final_color = texture(textures[3], f_in.uv);
-        break;
-      case 4:
-        final_color = texture(textures[4], f_in.uv);
-        break;
-      case 5:
-        final_color = texture(textures[5], f_in.uv);
-        break;
-      case 6:
-        final_color = texture(textures[6], f_in.uv);
-        break;
-      case 7:
-        final_color = texture(textures[7], f_in.uv);
-        break;
-      case 8:
-        final_color = texture(textures[8], f_in.uv);
-        break;
-      case 9:
-        final_color = texture(textures[9], f_in.uv);
-        break;
-      case 10:
-        final_color = texture(textures[10], f_in.uv);
-        break;
-    }
+    final_color = texture(textures[tid], f_in.uv);
   }
 
   if (final_color.a < 0.1)
