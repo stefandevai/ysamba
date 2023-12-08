293a69df618e010fc7854739baf05d8b8e076b1b
Stefan Devai <stefandevai@posteo.me>
2023-12-08 04:04:24 +0100

    Move color contruction to initialization list

--- src/graphics/color.cpp
+++ src/graphics/color.cpp
@@ -2,16 +2,17 @@
 
 namespace dl
 {
-Color::Color(const uint32_t color) : int_color(color)
+Color::Color(const uint32_t color)
+    : int_color(color),
+      rgba_color(RGBA{uint8_t((int_color & 0xFF000000) >> 24),
+                      uint8_t((int_color & 0x00FF0000) >> 16),
+                      uint8_t((int_color & 0x0000FF00) >> 8),
+                      uint8_t((int_color & 0x000000FF))})
 {
-  rgba_color.r = (int_color & 0xFF000000) >> 24;
-  rgba_color.g = (int_color & 0x00FF0000) >> 16;
-  rgba_color.b = (int_color & 0x0000FF00) >> 8;
-  rgba_color.a = (int_color & 0x000000FF);
 }
 
-Color::Color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a) : rgba_color(RGBA{r, g, b, a})
+Color::Color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a)
+    : int_color(a << 24 | b << 16 | g << 8 | r), rgba_color(RGBA{r, g, b, a})
 {
-  int_color = a << 24 | b << 16 | g << 8 | r;
 }
 }  // namespace dl
