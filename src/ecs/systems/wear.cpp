40b240965f2b451942284c1cbf2849e153d4084c
Stefan Devai <stefandevai@posteo.me>
2023-12-17 14:56:45 +0100

    Wear item only if agent contains body parts

--- src/ecs/systems/wear.cpp
+++ src/ecs/systems/wear.cpp
@@ -3,6 +3,8 @@
 #include <spdlog/spdlog.h>
 
 #include "ecs/components/action_wear.hpp"
+#include "ecs/components/biology.hpp"
+#include "ecs/components/item.hpp"
 #include "ecs/components/position.hpp"
 #include "ecs/components/weared_items.hpp"
 #include "world/world.hpp"
@@ -10,9 +12,7 @@
 namespace dl
 {
 const auto stop_wear = [](entt::registry& registry, const entt::entity entity, const Job* job) {
-  spdlog::debug("HEREE7");
   registry.remove<ActionWear>(entity);
-  spdlog::debug("HEREE8");
   job->status = JobStatus::Finished;
 };
 
@@ -20,22 +20,19 @@ WearSystem::WearSystem(World& world) : m_world(world) {}
 
 void WearSystem::update(entt::registry& registry)
 {
-  auto view = registry.view<ActionWear, const Position>();
+  auto view = registry.view<ActionWear, Biology, const Position>();
   for (const auto entity : view)
   {
-    spdlog::debug("HEREE1");
     auto& action_wear = registry.get<ActionWear>(entity);
     const auto& job = action_wear.job;
     const auto& target = job->target;
     const entt::entity item = static_cast<entt::entity>(target.id);
-    spdlog::debug("HEREE2");
 
     if (!registry.valid(item))
     {
       stop_wear(registry, entity, job);
       continue;
     }
-    spdlog::debug("HEREE3");
 
     // Check if target tile is still there
     if (!m_world.spatial_hash.has(item, target.position.x, target.position.y))
@@ -43,16 +40,30 @@ void WearSystem::update(entt::registry& registry)
       stop_wear(registry, entity, job);
       continue;
     }
-    spdlog::debug("HEREE4");
 
-    registry.remove<Position>(item);
-    spdlog::debug("HEREE5");
-    registry.remove<Visibility>(item);
+    auto& item_component = registry.get<Item>(item);
+    const auto& item_data = m_world.get_item_data(item_component.id);
+    const auto& biology = registry.get<Biology>(entity);
 
-    spdlog::debug("HEREE6");
-    auto& weared_items = registry.get<WearedItems>(entity);
-    spdlog::debug("HEREE9");
-    weared_items.items.push_back(item);
+    const auto& body_parts = biology.body_parts;
+    bool has_all_body_parts = true;
+
+    for (const auto body_part : item_data.weared_on)
+    {
+      if (std::find(body_parts.begin(), body_parts.end(), body_part) == body_parts.end())
+      {
+        has_all_body_parts = false;
+      }
+    }
+
+    if (has_all_body_parts)
+    {
+      registry.remove<Position>(item);
+      registry.remove<Visibility>(item);
+
+      auto& weared_items = registry.get<WearedItems>(entity);
+      weared_items.items.push_back(item);
+    }
 
     stop_wear(registry, entity, job);
   }
