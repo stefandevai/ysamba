45c5662703096e35e802d4c95092c5773789cf1e
Stefan Devai <stefandevai@posteo.me>
2024-02-29 14:33:06 +0100

    Improve NinePatch sprites initialization

--- src/graphics/nine_patch.cpp
+++ src/graphics/nine_patch.cpp
@@ -29,109 +29,86 @@ void NinePatch::generate_patches()
   const float uv_border_height = border / static_cast<float>(texture_size.y);
 
   // Top left patch
-  border_patches[0].resource_id = resource_id;
-  border_patches[0].texture = texture;
-  border_patches[0].color = color;
-  border_patches[0].size = glm::vec2(border, border);
-  // border_patches[0].set_size(border, border);
+  border_patches[0] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(border, border),
+  };
   border_patches[0].set_uv(uv_top, uv_left, uv_top + uv_border_height, uv_left + uv_border_width);
 
-  // border_patches[0].uv_coordinates = std::array<glm::vec2, 4>{
-  //     glm::vec2{uv_left, uv_top},
-  //     glm::vec2{uv_left + uv_border_width, uv_top},
-  //     glm::vec2{uv_left + uv_border_width, uv_top + uv_border_height},
-  //     glm::vec2{uv_left, uv_top + uv_border_height},
-  // };
-
-  // border_patches[0].uv_coordinates = std::array<glm::vec2, 4>{
-  //     glm::vec2{left, top},
-  //     glm::vec2{right, top},
-  //     glm::vec2{right, bottom},
-  //     glm::vec2{left, bottom},
-  // };
-
   // Top center patch
-  border_patches[1].resource_id = resource_id;
-  border_patches[1].texture = texture;
-  border_patches[1].color = color;
-  border_patches[1].size = glm::vec2(size.x - border * 2, border);
-  // border_patches[1].set_size(size.x - border * 2, border);
+  border_patches[1] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(size.x - border * 2, border),
+  };
   border_patches[1].set_uv(uv_top, uv_left + uv_border_width, uv_top + uv_border_height, uv_right - uv_border_width);
-  // border_patches[1].uv_coordinates = std::array<glm::vec2, 4>{
-  //     glm::vec2{uv_left + uv_border_width, uv_top},
-  //     glm::vec2{uv_right - uv_border_width, uv_top},
-  //     glm::vec2{uv_right - uv_border_width, uv_top + uv_border_height},
-  //     glm::vec2{uv_left + uv_border_width, uv_top + uv_border_height},
-  // };
 
   // Top right patch
-  border_patches[2].resource_id = resource_id;
-  border_patches[2].texture = texture;
-  border_patches[2].color = color;
-  border_patches[2].size = glm::vec2(border, border);
-  // border_patches[2].set_size(border, border);
+  border_patches[2] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(border, border),
+  };
   border_patches[2].set_uv(uv_top, uv_right - uv_border_width, uv_top + uv_border_height, uv_right);
-  // border_patches[2].uv_coordinates = std::array<glm::vec2, 4>{
-  //     glm::vec2{uv_right - uv_border_width, uv_top},
-  //     glm::vec2{uv_right, uv_top},
-  //     glm::vec2{uv_right, uv_top + uv_border_height},
-  //     glm::vec2{uv_right - uv_border_width, uv_top + uv_border_height},
-  // };
 
   // Center right patch
-  border_patches[3].resource_id = resource_id;
-  border_patches[3].texture = texture;
-  border_patches[3].color = color;
-  border_patches[3].size = glm::vec2(border, size.y - border * 2);
-  // border_patches[3].set_size(border, size.y - border * 2);
+  border_patches[3] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(border, size.y - border * 2),
+  };
   border_patches[3].set_uv(
       uv_top + uv_border_height, uv_right - uv_border_width, uv_bottom - uv_border_height, uv_right);
-  // border_patches[0].uv_coordinates = std::array<glm::vec2, 4>{
-  //     glm::vec2{uv_right - uv_border_width, uv_top + uv_border_height},
-  //     glm::vec2{uv_right, uv_top + uv_border_height},
-  //     glm::vec2{uv_right, uv_bottom - uv_border_height},
-  //     glm::vec2{uv_right - uv_border_width, uv_bottom - uv_border_height},
-  // };
 
   // Bottom right patch
-  border_patches[4].resource_id = resource_id;
-  border_patches[4].texture = texture;
-  border_patches[4].color = color;
-  border_patches[4].size = glm::vec2(border, border);
-  // border_patches[4].set_size(border, border);
+  border_patches[4] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(border, border),
+  };
   border_patches[4].set_uv(uv_bottom - uv_border_height, uv_right - uv_border_width, uv_bottom, uv_right);
 
   // Bottom center patch
-  border_patches[5].resource_id = resource_id;
-  border_patches[5].texture = texture;
-  border_patches[5].color = color;
-  border_patches[5].size = glm::vec2(size.x - border * 2, border);
-  // border_patches[5].set_size(size.x - border * 2, border);
+  border_patches[5] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(size.x - border * 2, border),
+  };
   border_patches[5].set_uv(
       uv_bottom - uv_border_height, uv_left + uv_border_width, uv_bottom, uv_right - uv_border_width);
 
   // Bottom left patch
-  border_patches[6].resource_id = resource_id;
-  border_patches[6].texture = texture;
-  border_patches[6].color = color;
-  border_patches[6].size = glm::vec2(border, border);
-  // border_patches[6].set_size(border, border);
+  border_patches[6] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(border, border),
+  };
   border_patches[6].set_uv(uv_bottom - uv_border_height, uv_left, uv_bottom, uv_left + uv_border_width);
 
   // Center left patch
-  border_patches[7].resource_id = resource_id;
-  border_patches[7].texture = texture;
-  border_patches[7].color = color;
-  border_patches[7].size = glm::vec2(border, size.y - border * 2);
-  // border_patches[7].set_size(border, size.y - border * 2);
+  border_patches[7] = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(border, size.y - border * 2),
+  };
   border_patches[7].set_uv(uv_top + uv_border_height, uv_left, uv_bottom - uv_border_height, uv_left + uv_border_width);
 
   // Center patch
-  center_patch.resource_id = resource_id;
-  center_patch.texture = texture;
-  center_patch.color = color;
-  center_patch.size = glm::vec2(size.x - border * 2, size.y - border * 2);
-  // center_patch.set_size(size.x - border * 2, size.y - border * 2);
+  center_patch = {
+      .resource_id = resource_id,
+      .texture = texture,
+      .color = color,
+      .size = glm::vec2(size.x - border * 2, size.y - border * 2),
+  };
   center_patch.set_uv(
       uv_top + uv_border_height, uv_left + uv_border_width, uv_bottom - uv_border_height, uv_right - uv_border_width);
 
