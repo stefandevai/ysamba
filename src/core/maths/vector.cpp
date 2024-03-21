dd656f3bacb2f4af8c99a46222d37884e8939c4d
Stefan Devai <stefandevai@posteo.me>
2024-03-21 02:05:36 +0100

    Add basic math operations to vector class

--- src/core/maths/vector.cpp
+++ src/core/maths/vector.cpp
@@ -2,6 +2,10 @@
 
 namespace dl
 {
+Vector2::Vector2(const double x, const double y) : x(x), y(y) {}
+
+Vector2::Vector2(const int x, const int y) : x(static_cast<double>(x)), y(static_cast<double>(y)) {}
+
 Vector2::Vector2(const Vector2& other)
 {
   x = other.x;
@@ -27,6 +31,46 @@ Vector2& Vector2::operator=(const Vector2i& rhs)
   y = rhs.y;
   return *this;
 }
+
+Vector2 Vector2::operator+(const Vector2& rhs)
+{
+  return Vector2{x + rhs.x, y + rhs.y};
+}
+
+Vector2& Vector2::operator+=(const Vector2& rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+Vector2 Vector2::operator-(const Vector2& rhs)
+{
+  return Vector2{x - rhs.x, y - rhs.y};
+}
+
+Vector2& Vector2::operator-=(const Vector2& rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  return *this;
+}
+
+Vector2 Vector2::operator*(const double rhs)
+{
+  return Vector2{x * rhs, y * rhs};
+}
+
+Vector2& Vector2::operator*=(const double rhs)
+{
+  x *= rhs;
+  y *= rhs;
+  return *this;
+}
+
+Vector2i::Vector2i(const int x, const int y) : x(x), y(y) {}
+
+Vector2i::Vector2i(const double x, const double y) : x(static_cast<int>(x)), y(static_cast<int>(y)) {}
 
 Vector2i::Vector2i(const Vector2i& other)
 {
@@ -54,6 +98,49 @@ Vector2i& Vector2i::operator=(const Vector2& rhs)
   x = rhs.x;
   y = rhs.y;
   return *this;
+}
+
+Vector2i Vector2i::operator+(const Vector2i& rhs)
+{
+  return Vector2i{x + rhs.x, y + rhs.y};
+}
+
+Vector2i& Vector2i::operator+=(const Vector2i& rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+Vector2i Vector2i::operator-(const Vector2i& rhs)
+{
+  return Vector2i{x - rhs.x, y - rhs.y};
+}
+
+Vector2i& Vector2i::operator-=(const Vector2i& rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  return *this;
+}
+
+Vector2i Vector2i::operator*(const int rhs)
+{
+  return Vector2i{x * rhs, y * rhs};
+}
+
+Vector2i& Vector2i::operator*=(const int rhs)
+{
+  x *= rhs;
+  y *= rhs;
+  return *this;
+}
+
+Vector3i::Vector3i(const int x, const int y, const int z) : x(x), y(y), z(z) {}
+
+Vector3i::Vector3i(const double x, const double y, const double z)
+  : x(static_cast<int>(x)), y(static_cast<int>(y)), z(static_cast<int>(z))
+{
 }
 
 Vector3i::Vector3i(const Vector3i& other)
@@ -88,12 +175,50 @@ Vector3i& Vector3i::operator=(const Vector3& rhs)
   return *this;
 }
 
-Vector3& Vector3::operator+=(const Vector3& rhs)
+Vector3i Vector3i::operator+(const Vector3i& rhs)
 {
-  this->x += rhs.x;
-  this->y += rhs.y;
-  this->z += rhs.z;
+  return Vector3i{x + rhs.x, y + rhs.y, z + rhs.z};
+}
+
+Vector3i& Vector3i::operator+=(const Vector3i& rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  z += rhs.z;
   return *this;
+}
+
+Vector3i Vector3i::operator-(const Vector3i& rhs)
+{
+  return Vector3i{x - rhs.x, y - rhs.y, z - rhs.z};
+}
+
+Vector3i& Vector3i::operator-=(const Vector3i& rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  z -= rhs.z;
+  return *this;
+}
+
+Vector3i Vector3i::operator*(const int rhs)
+{
+  return Vector3i{x * rhs, y * rhs, z * rhs};
+}
+
+Vector3i& Vector3i::operator*=(const int rhs)
+{
+  x *= rhs;
+  y *= rhs;
+  z *= rhs;
+  return *this;
+}
+
+Vector3::Vector3(const double x, const double y, const double z) : x(x), y(y), z(z) {}
+
+Vector3::Vector3(const int x, const int y, const int z)
+  : x(static_cast<double>(x)), y(static_cast<double>(y)), z(static_cast<double>(z))
+{
 }
 
 Vector3::Vector3(const Vector3& other)
@@ -128,12 +253,142 @@ Vector3& Vector3::operator=(const Vector3i& rhs)
   return *this;
 }
 
+Vector3 Vector3::operator+(const Vector3& rhs)
+{
+  return Vector3{x + rhs.x, y + rhs.y, z + rhs.z};
+}
+
+Vector3& Vector3::operator+=(const Vector3& rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  z += rhs.z;
+  return *this;
+}
+
+Vector3 Vector3::operator-(const Vector3& rhs)
+{
+  return Vector3{x - rhs.x, y - rhs.y, z - rhs.z};
+}
+
+Vector3& Vector3::operator-=(const Vector3& rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  z -= rhs.z;
+  return *this;
+}
+
+Vector3 Vector3::operator*(const double rhs)
+{
+  return Vector3{x * rhs, y * rhs, z * rhs};
+}
+
+Vector3& Vector3::operator*=(const double rhs)
+{
+  x *= rhs;
+  y *= rhs;
+  z *= rhs;
+  return *this;
+}
+
+Vector4d::Vector4d(const double x, const double y, const double z, const double w) : x(x), y(y), z(z), w(w) {}
+
+Vector4d::Vector4d(const int x, const int y, const int z, const int w)
+  : x(static_cast<double>(x)), y(static_cast<double>(y)), z(static_cast<double>(z)), w(static_cast<double>(w))
+{
+}
+
+Vector4d::Vector4d(const Vector4d& other)
+{
+  x = other.x;
+  y = other.y;
+  z = other.z;
+  w = other.w;
+}
+
+Vector4d::Vector4d(const Vector4i& other)
+{
+  x = static_cast<double>(other.x);
+  y = static_cast<double>(other.y);
+  z = static_cast<double>(other.z);
+  w = static_cast<double>(other.w);
+}
+
+bool Vector4d::operator==(const Vector4d& rhs) const { return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w; }
+
+Vector4d& Vector4d::operator=(const Vector4d& rhs)
+{
+  x = rhs.x;
+  y = rhs.y;
+  z = rhs.z;
+  w = rhs.w;
+  return *this;
+}
+
+Vector4d Vector4d::operator+(const Vector4d& rhs)
+{
+  return Vector4d{x + rhs.x, y + rhs.y, z + rhs.z, w + rhs.w};
+}
+
+Vector4d& Vector4d::operator+=(const Vector4d& rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  z += rhs.z;
+  w += rhs.w;
+  return *this;
+}
+
+Vector4d Vector4d::operator-(const Vector4d& rhs)
+{
+  return Vector4d{x - rhs.x, y - rhs.y, z - rhs.z, w - rhs.w};
+}
+
+Vector4d& Vector4d::operator-=(const Vector4d& rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  z -= rhs.z;
+  w -= rhs.w;
+  return *this;
+}
+
+Vector4d Vector4d::operator*(const double rhs)
+{
+  return Vector4d{x * rhs, y * rhs, z * rhs, w * rhs};
+}
+
+Vector4d& Vector4d::operator*=(const double rhs)
+{
+  x *= rhs;
+  y *= rhs;
+  z *= rhs;
+  w *= rhs;
+  return *this;
+}
+
+Vector4i::Vector4i(const int x, const int y, const int z, const int w) : x(x), y(y), z(z), w(w) {}
+
+Vector4i::Vector4i(const double x, const double y, const double z, const double w)
+  : x(static_cast<int>(x)), y(static_cast<int>(y)), z(static_cast<int>(z)), w(static_cast<int>(w))
+{
+}
+
 Vector4i::Vector4i(const Vector4i& other)
 {
   x = other.x;
   y = other.y;
   z = other.z;
   w = other.w;
+}
+
+Vector4i::Vector4i(const Vector4d& other)
+{
+  x = static_cast<int>(other.x);
+  y = static_cast<int>(other.y);
+  z = static_cast<int>(other.z);
+  w = static_cast<int>(other.w);
 }
 
 bool Vector4i::operator==(const Vector4i& rhs) const { return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w; }
@@ -144,6 +399,48 @@ Vector4i& Vector4i::operator=(const Vector4i& rhs)
   y = rhs.y;
   z = rhs.z;
   w = rhs.w;
+  return *this;
+}
+
+Vector4i Vector4i::operator+(const Vector4i& rhs)
+{
+  return Vector4i{x + rhs.x, y + rhs.y, z + rhs.z, w + rhs.w};
+}
+
+Vector4i& Vector4i::operator+=(const Vector4i& rhs)
+{
+  x += rhs.x;
+  y += rhs.y;
+  z += rhs.z;
+  w += rhs.w;
+  return *this;
+}
+
+Vector4i Vector4i::operator-(const Vector4i& rhs)
+{
+  return Vector4i{x - rhs.x, y - rhs.y, z - rhs.z, w - rhs.w};
+}
+
+Vector4i& Vector4i::operator-=(const Vector4i& rhs)
+{
+  x -= rhs.x;
+  y -= rhs.y;
+  z -= rhs.z;
+  w -= rhs.w;
+  return *this;
+}
+
+Vector4i Vector4i::operator*(const int rhs)
+{
+  return Vector4i{x * rhs, y * rhs, z * rhs, w * rhs};
+}
+
+Vector4i& Vector4i::operator*=(const int rhs)
+{
+  x *= rhs;
+  y *= rhs;
+  z *= rhs;
+  w *= rhs;
   return *this;
 }
 
--- src/core/maths/vector.hpp
+++ src/core/maths/vector.hpp
@@ -3,6 +3,8 @@
 namespace dl
 {
 struct Vector2i;
+struct Vector3i;
+struct Vector4i;
 
 struct Vector2
 {
@@ -10,13 +12,20 @@ struct Vector2
   double y = 0.0;
 
   Vector2() = default;
-  Vector2(const double x, const double y) : x(x), y(y) {}
-  Vector2(const int x, const int y) : x(static_cast<double>(x)), y(static_cast<double>(y)) {}
+  Vector2(const double x, const double y);
+  Vector2(const int x, const int y);
   Vector2(const Vector2& other);
   Vector2(const Vector2i& other);
+
   bool operator==(const Vector2& rhs) const;
   Vector2& operator=(const Vector2& rhs);
   Vector2& operator=(const Vector2i& rhs);
+  Vector2 operator+(const Vector2& rhs);
+  Vector2& operator+=(const Vector2& rhs);
+  Vector2 operator-(const Vector2& rhs);
+  Vector2& operator-=(const Vector2& rhs);
+  Vector2 operator*(const double rhs);
+  Vector2& operator*=(const double rhs);
 };
 
 template <typename Archive>
@@ -31,46 +40,26 @@ struct Vector2i
   int y = 0;
 
   Vector2i() = default;
-  Vector2i(const int x, const int y) : x(x), y(y) {}
-  Vector2i(const double x, const double y) : x(static_cast<int>(x)), y(static_cast<int>(y)) {}
+  Vector2i(const int x, const int y);
+  Vector2i(const double x, const double y);
   Vector2i(const Vector2i& other);
   Vector2i(const Vector2& other);
+
   bool operator==(const Vector2i& rhs) const;
   Vector2i& operator=(const Vector2i& rhs);
   Vector2i& operator=(const Vector2& rhs);
+  Vector2i operator+(const Vector2i& rhs);
+  Vector2i& operator+=(const Vector2i& rhs);
+  Vector2i operator-(const Vector2i& rhs);
+  Vector2i& operator-=(const Vector2i& rhs);
+  Vector2i operator*(const int rhs);
+  Vector2i& operator*=(const int rhs);
 };
 
 template <typename Archive>
 void serialize(Archive& archive, Vector2i& v)
 {
   archive(v.x, v.y);
-}
-
-struct Vector3;
-
-struct Vector3i
-{
-  int x = 0;
-  int y = 0;
-  int z = 0;
-
-  Vector3i() = default;
-  Vector3i(const int x, const int y, const int z) : x(x), y(y), z(z) {}
-  Vector3i(const double x, const double y, const double z)
-      : x(static_cast<int>(x)), y(static_cast<int>(y)), z(static_cast<int>(z))
-  {
-  }
-  Vector3i(const Vector3i& other);
-  Vector3i(const Vector3& other);
-  bool operator==(const Vector3i& rhs) const;
-  Vector3i& operator=(const Vector3i& rhs);
-  Vector3i& operator=(const Vector3& rhs);
-};
-
-template <typename Archive>
-void serialize(Archive& archive, Vector3i& v)
-{
-  archive(v.x, v.y, v.z);
 }
 
 struct Vector3
@@ -80,23 +69,84 @@ struct Vector3
   double z = 0.0;
 
   Vector3() = default;
-  Vector3(const double x, const double y, const double z) : x(x), y(y), z(z) {}
-  Vector3(const int x, const int y, const int z)
-      : x(static_cast<double>(x)), y(static_cast<double>(y)), z(static_cast<double>(z))
-  {
-  }
+  Vector3(const double x, const double y, const double z);
+  Vector3(const int x, const int y, const int z);
   Vector3(const Vector3& other);
   Vector3(const Vector3i& other);
+
   bool operator==(const Vector3& rhs) const;
   Vector3& operator=(const Vector3& rhs);
   Vector3& operator=(const Vector3i& rhs);
+  Vector3 operator+(const Vector3& rhs);
   Vector3& operator+=(const Vector3& rhs);
+  Vector3 operator-(const Vector3& rhs);
+  Vector3& operator-=(const Vector3& rhs);
+  Vector3 operator*(const double rhs);
+  Vector3& operator*=(const double rhs);
 };
 
 template <typename Archive>
 void serialize(Archive& archive, Vector3& v)
 {
   archive(v.x, v.y, v.z);
+}
+
+struct Vector3i
+{
+  int x = 0;
+  int y = 0;
+  int z = 0;
+
+  Vector3i() = default;
+  Vector3i(const int x, const int y, const int z);
+  Vector3i(const double x, const double y, const double z);
+  Vector3i(const Vector3i& other);
+  Vector3i(const Vector3& other);
+
+  bool operator==(const Vector3i& rhs) const;
+  Vector3i& operator=(const Vector3i& rhs);
+  Vector3i& operator=(const Vector3& rhs);
+  Vector3i operator+(const Vector3i& rhs);
+  Vector3i& operator+=(const Vector3i& rhs);
+  Vector3i operator-(const Vector3i& rhs);
+  Vector3i& operator-=(const Vector3i& rhs);
+  Vector3i operator*(const int rhs);
+  Vector3i& operator*=(const int rhs);
+};
+
+template <typename Archive>
+void serialize(Archive& archive, Vector3i& v)
+{
+  archive(v.x, v.y, v.z);
+}
+
+struct Vector4d
+{
+  double x = 0;
+  double y = 0;
+  double z = 0;
+  double w = 0;
+
+  Vector4d() = default;
+  Vector4d(const double x, const double y, const double z, const double w);
+  Vector4d(const int x, const int y, const int z, const int w);
+  Vector4d(const Vector4d& other);
+  Vector4d(const Vector4i& other);
+
+  bool operator==(const Vector4d& rhs) const;
+  Vector4d& operator=(const Vector4d& rhs);
+  Vector4d operator+(const Vector4d& rhs);
+  Vector4d& operator+=(const Vector4d& rhs);
+  Vector4d operator-(const Vector4d& rhs);
+  Vector4d& operator-=(const Vector4d& rhs);
+  Vector4d operator*(const double rhs);
+  Vector4d& operator*=(const double rhs);
+};
+
+template <typename Archive>
+void serialize(Archive& archive, Vector4d& v)
+{
+  archive(v.x, v.y, v.z, v.w);
 }
 
 struct Vector4i
@@ -107,14 +157,19 @@ struct Vector4i
   int w = 0;
 
   Vector4i() = default;
-  Vector4i(const int x, const int y, const int z, const int w) : x(x), y(y), z(z), w(w) {}
-  Vector4i(const double x, const double y, const double z, const int w)
-      : x(static_cast<int>(x)), y(static_cast<int>(y)), z(static_cast<int>(z)), w(static_cast<int>(w))
-  {
-  }
+  Vector4i(const int x, const int y, const int z, const int w);
+  Vector4i(const double x, const double y, const double z, const double w);
   Vector4i(const Vector4i& other);
+  Vector4i(const Vector4d& other);
+
   bool operator==(const Vector4i& rhs) const;
   Vector4i& operator=(const Vector4i& rhs);
+  Vector4i operator+(const Vector4i& rhs);
+  Vector4i& operator+=(const Vector4i& rhs);
+  Vector4i operator-(const Vector4i& rhs);
+  Vector4i& operator-=(const Vector4i& rhs);
+  Vector4i operator*(const int rhs);
+  Vector4i& operator*=(const int rhs);
 };
 
 template <typename Archive>
