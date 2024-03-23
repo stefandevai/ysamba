9288838430051c99f05358f872c6f09ebcc63e12
Stefan Devai <stefandevai@posteo.me>
2024-03-23 17:59:00 +0100

    Modify textures json data to account for multiple cube faces

---
+++ data/textures/refactor.py
@@ -0,0 +1,55 @@
+import json
+
+data_filepath = 'characters_old.json'
+output_filepath = 'characters.json'
+
+file = open(data_filepath, 'r')
+data = json.load(file)
+
+def find_frame_by_id(game_id):
+    for frame in data['frames']:
+        if frame['game_id'] == game_id:
+            return frame['frame']
+    return -1
+
+for frame in data['frames']:
+    if 'angle' not in frame and 'front_face_id' not in frame:
+        continue
+
+    frame_id = frame['frame']
+    front_face_frame = -1
+    angle = ''
+
+    if 'front_face_id' in frame:
+        front_face_id = frame['front_face_id']
+        front_face_frame = find_frame_by_id(front_face_id)
+
+        if front_face_frame == -1:
+            raise ValueError('Front face not found for frame ' + str(frame_id))
+
+        del frame['front_face_id']
+
+    if 'angle' in frame:
+        angle = frame['angle']
+        del frame['angle']
+
+    faces = { }
+
+    if front_face_frame != -1:
+        faces['top'] = frame_id
+        faces['front'] = front_face_frame
+    elif angle == 'orthogonal':
+        faces['front'] = frame_id
+    else:
+        faces['top'] = frame_id
+
+    frame['faces'] = faces
+
+for frame in data['frames']:
+    if 'faces' not in frame:
+        continue
+
+    del frame['frame']
+
+with open(output_filepath, 'w') as output_file:
+    json.dump(data, output_file, indent=4)
--- src/graphics/frame_angle.hpp
+++ src/graphics/frame_angle.hpp
@@ -5,3 +5,18 @@ enum class FrameAngle
   Parallel,
   Orthogonal,
 };
+
+// Defines in which face a sprite can be rendered.
+// Bottom, Front, Back, Top, Left, Right are the faces of a cube.
+// CenterHorizontal, CenterVertical render the sprite in the center of the cube area.
+enum class RenderFace
+{
+  Bottom,
+  Front,
+  Back,
+  Top,
+  Left,
+  Right,
+  CenterHorizontal,
+  CenterVertical,
+};
